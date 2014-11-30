c7af94097dc49d40cee7599d75c19b388e8ed5c3
Luiz Pericolo <luizpericolo@gmail.com>
2014-11-30 20:35:38 -0200

    Adicionando urls e mapeando elas a views.

--- steam_game_recommender/steam_game_recommender/urls.py
+++ steam_game_recommender/steam_game_recommender/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from steam_game_recommender.views import initial, get_recommendation
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^recommender/$', initial),
+    url(r'^get_recommendation$', get_recommendation),
 )
