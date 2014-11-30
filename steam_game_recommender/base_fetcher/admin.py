787998f01d869d7b49431a51aa328ea26836aabb
Luiz Pericolo <luizpericolo@gmail.com>
2014-11-30 20:34:16 -0200

    Registrando os models no admin para poder acompanhar melhor.

--- steam_game_recommender/base_fetcher/admin.py
+++ steam_game_recommender/base_fetcher/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from base_fetcher.models import SteamUser, SteamGame, SteamUserGame
 
 # Register your models here.
+
+class SteamUserAdmin(admin.ModelAdmin):
+	pass
+
+class SteamGameAdmin(admin.ModelAdmin):
+	pass
+
+class SteamUserGameAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(SteamUser, SteamUserAdmin)
+admin.site.register(SteamGame, SteamGameAdmin)
+admin.site.register(SteamUserGame, SteamUserGameAdmin)
