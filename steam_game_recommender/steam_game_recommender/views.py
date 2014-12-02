2bf46c5426c84715047bb8a2e6b24d5c34816d58
Luiz Pericolo <luizpericolo@gmail.com>
2014-12-02 16:10:04 -0200

    recuperando jogos presentes na wishlist do usuário e mostrando na tela

--- steam_game_recommender/steam_game_recommender/views.py
+++ steam_game_recommender/steam_game_recommender/views.py
@@ -10,9 +10,13 @@ def initial(request):
 	return TemplateResponse(request, 'index.html', {'form': form})
 
 def get_recommendation(request):
+	from utils import SteamWishlistFetcher
 	steam_name = request.POST['steam_name']
 	crawler = SteamIdConverterCrawler()
 	steam_id = crawler.get_steamid64(steam_name)
+
+	wishlistFetcher = SteamWishlistFetcher()
+	wishlist_games = wishlistFetcher.retrieve_wishlist_for_steamname(steamname=steam_name)
 
 	games = [
 		{'name': 'Counter-Strike','url':'http://store.steampowered.com/app/10'},
@@ -20,4 +24,4 @@ def get_recommendation(request):
 		{'name': 'Half-Life 2','url': 'http://store.steampowered.com/app/220'},
 	]
 
-	return TemplateResponse(request, 'results.html', {'steam_name': request.POST['steam_name'], 'games': games})
+	return TemplateResponse(request, 'results.html', {'steam_name': request.POST['steam_name'], 'games': games, "wishlist_games": wishlist_games})
