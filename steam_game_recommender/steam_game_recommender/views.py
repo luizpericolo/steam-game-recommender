accfc8d16c3e058f076c6fb00dfbfe8c92be865a
Luiz Pericolo <luizpericolo@gmail.com>
2014-12-07 23:20:05 -0200

    Fazendo o recomendador funcionar para os dados da comunidade.

--- steam_game_recommender/steam_game_recommender/views.py
+++ steam_game_recommender/steam_game_recommender/views.py
@@ -10,18 +10,48 @@ def initial(request):
 	return TemplateResponse(request, 'index.html', {'form': form})
 
 def get_recommendation(request):
-	from utils import SteamWishlistFetcher
+	import json, re
+	import graphlab as gl
+	from django.conf import settings
+	from utils import SteamWishlistFetcher, FriendListFetcher
+
 	steam_name = request.POST['steam_name']
+
+	if re.compile(r"\d+").match(steam_name):
+		steam_id = steam_name
+
 	crawler = SteamIdConverterCrawler()
-	steam_id = crawler.get_steamid64(steam_name)
+	steam_id = crawler.get_steamid64(steam_name=steam_name)
+
+	friendListFetcher = FriendListFetcher(app_key=settings.API_KEY)
+	friends = friendListFetcher.get_friends(steamid=steam_id)
+
+	# Gerar a base a partir dos ids dos amigos e jogar para o
+	# graphlab.
+	games = open("./games_list.json", 'r')
+	games_list = json.load(games)
+	games.close()
+
+	data = gl.SFrame.read_csv("./scored_output.csv",column_type_hints={"score": int})
+
+	# Definindo para quem devemos fazer sugestões
+	users = [steam_id]
+
+	model = gl.recommender.create(data, user_id="steam_id", item_id="app_id", target="score")
+	#model = gl.ranking_factorization_recommender.create(data, user_id="steam_id", item_id="app_id", target="score")
+	recommended_games = model.recommend(users=users, k=5)
+
+	games = []
+
+	for game in recommended_games:
+		game = {
+			'name': games_list[game.get('app_id')],
+			'url': "http://store.steampowered.com/app/{}".format(game.get('app_id')),
+			'score': game.get('score')
+		}
+		games.append(game)
 
 	wishlistFetcher = SteamWishlistFetcher()
-	wishlist_games = wishlistFetcher.retrieve_wishlist_for_steamname(steamname=steam_name)
-
-	games = [
-		{'name': 'Counter-Strike','url':'http://store.steampowered.com/app/10'},
-		{'name': 'BioShock' ,'url': 'http://store.steampowered.com/app/7670'},
-		{'name': 'Half-Life 2','url': 'http://store.steampowered.com/app/220'},
-	]
+	wishlist_games = wishlistFetcher.retrieve_wishlist_for_steam_id(steam_id=steam_id)
 
 	return TemplateResponse(request, 'results.html', {'steam_name': request.POST['steam_name'], 'games': games, "wishlist_games": wishlist_games})
