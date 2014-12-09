794903f09fd67057cfec48ac3f4b5fb342d11edb
Luiz Pericolo <luizpericolo@gmail.com>
2014-12-08 23:34:00 -0200

    Removendo label chata do input e adicionando um placeholder

--- steam_game_recommender/steam_game_recommender/forms.py
+++ steam_game_recommender/steam_game_recommender/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SteamGameRecommenderForm(forms.Form):
-	steam_name = forms.CharField(label='Steam Name', max_length=255)
+	steam_name = forms.CharField(label="", max_length=255, widget=forms.TextInput(attrs={'placeholder': 'SteamID or Steam Name...'}))
